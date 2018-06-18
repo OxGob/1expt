16ad183231ed4b58fb00ee777ca5b6a3ea1bb55b
oxgob <arvingo@yahoo.co.uk>
2018-06-18 19:01:05 +0100

    Added Criteria to tabs

--- genexpt/src/pages/index.vue
+++ genexpt/src/pages/index.vue
@@ -2,6 +2,7 @@
   <q-page>
       <q-tabs color="secondary" glossy align="justify">
         <q-tab slot="title" default name="generalities" icon="subject" label="Generalities" />
+        <q-tab slot="title" name="criteria" icon="fingerprint" label="Inclusion Criteria" />
         <q-tab slot="title" name="test" icon="verified_user" label="Test"/>
         <q-tab slot="title" name="test2" icon="verified_user" label="Tab Inv"/>
         <q-tab slot="title" name="test3" icon="verified_user" label="Tab Inst"/>
@@ -14,13 +15,16 @@
             </q-card-title>
             <q-card-main>
               <q-field label="Title" helper="Short name of the study">
-                <q-input v-model="studyTitle" @input="$v.studyTitle.$touch()" clearable></q-input>
+               <q-input v-model="studyTitle" @input="$v.studyTitle.$touch()" clearable />
                 <div v-if="$v.studyTitle.$dirty">
-                  <p v-if="!$v.studyTitle.required" class="text-negative">The title field is required!</p>
+                  <p v-if="!$v.studyTitle.required" class="q-mt-sm text-negative"> A title is required for the study. Please enter it.</p>
                 </div>
               </q-field>
               <q-field label="Description" helper="Layman's description of the study">
-                <q-input v-model="studyDescription" type="textarea" rows="7" clearable />
+                <q-input v-model="studyDescription" @input="$v.studyDescription.$touch()" type="textarea" rows="7" clearable />
+                 <div v-if="$v.studyDescription.$dirty">
+                  <p v-if="!$v.studyDescription.required" class="q-mt-sm text-negative">A description is required for the study. Please enter it.</p>
+                 </div>
               </q-field>
             </q-card-main>
           </q-card>
@@ -31,6 +35,9 @@
             </q-card-title>
             <q-card-main>
               <div v-for="(principalInvestigator, index) in principalInvestigators" :key="principalInvestigator.id">
+                <div>
+                  <q-btn class="vertical-top" v-show="index !==0" round size="sm" color="negative" icon="remove" @click="removeRowInvestigator(index)" />
+                </div>
                 <q-field label="Name & Title:" helper="Principal Investigator Name & Title">
                   <q-input v-model="principalInvestigator.nameTitleOfInvestigator" type="text" clearable />
                 </q-field>
@@ -40,9 +47,8 @@
                 <q-field label="Institution:" helper="Name of Institution involved in study">
                   <q-input v-model="principalInvestigator.institutionOfInvestigator" type="textarea" rows="2"  clearable />
                 </q-field>
-                <q-btn class="q-ml-md q-mt-lg" v-show="showRemoveButton" round size="sm" color="negative" icon="remove" @click="removeRowInvestigator(index)" />
-                <q-btn class="float-right q-mt-lg" round size="sm" color="primary" icon="add" @click="addRowInvestigator(index)" />
-                <q-card-separator class="q-mb-md q-mt-md"/>
+                <q-btn class="float-right q-mt-sm" round size="sm" color="primary" icon="add" @click="addRowInvestigator(index)" />
+                <q-card-separator class="q-mb-md q-mt-xl"/>
               </div>
             </q-card-main>
           </q-card>
@@ -53,6 +59,7 @@
             </q-card-title>
             <q-card-main>
               <div v-for="(institution, index) in institutions" :key="institution.id">
+                <q-btn class="vertical-top" v-show="index !==0" round size="sm" color="negative" icon="remove" @click="removeRowInstitution(index)" />
                 <q-field label="Name of Institution:" helper="Please enter the name of the institution">
                   <q-input v-model="institution.nameOfInstitution" type="text" clearable />
                 </q-field>
@@ -61,12 +68,11 @@
                 </q-field>
                 <q-field class="q-mt-md" label="Data Access:" helper="Data access per institution. Please select the level of access to grant or not.">
                   <q-radio v-model="institution.radioDataAccessInstitution" val="no" color="secondary" label="NO" />
-                  <q-radio v-model="institution.radioDataAccessInstitution" val="anon" color="anon" label="ANON" />
+                  <q-radio class="q-ml-md q-mr-md" v-model="institution.radioDataAccessInstitution" val="anon" color="anon" label="ANON" />
                   <q-radio v-model="institution.radioDataAccessInstitution" val="full" color="full" label="FULL" />
                 </q-field>
-                <q-btn class="q-ml-md q-mt-lg" v-show="showRemoveButton" round size="sm" color="negative" icon="remove" @click="removeRowInstitution(index)" />
-                <q-btn class="float-right q-mt-lg" round size="sm" color="primary" icon="add" @click="addRowInstitution(index)" />
-                <q-card-separator class="q-mb-md q-mt-md"/>
+                <q-btn class="float-right q-mt-sm" round size="sm" color="primary" icon="add" @click="addRowInstitution(index)" />
+                <q-card-separator class="q-mb-md q-mt-xl"/>
               </div>
             </q-card-main>
           </q-card>
@@ -83,7 +89,10 @@
                         <q-field label="Start Date" />
                         </div>
                         <div class="col-xs-4 col-md-4">
-                          <q-datetime class="q-ml-xl q-mb-lg" v-model="dateStart" type="date" format="D-MMM-YYYY" clearable />
+                          <q-datetime class="q-ml-xl q-mb-lg" v-model="dateStart" @input="$v.dateStart.$touch()" type="date" format="D-MMM-YYYY" clearable />
+                          <div v-if="$v.dateStart.$dirty">
+                            <p v-if="!$v.dateStart.required" class="q-mt-sm text-negative">The Start Date is required. Please enter it.</p>
+                          </div>
                         </div>
                     </div>
                   </div>
@@ -95,16 +104,98 @@
                         <q-field label="End Date" />
                         </div>
                         <div class="col-xs-4 col-md-4">
-                          <q-datetime class="q-ml-xl" v-model="dateEnd" type="date" format="D-MMM-YYYY" clearable />
+                          <q-datetime class="q-ml-xl" v-model="dateEnd" type="date" @input="checkEndDate(dateStart, dateEnd)" format="D-MMM-YYYY" clearable />
                         </div>
                     </div>
                   </div>
               </div>
             </q-card-main>
           </q-card>
+          <!-- Gen Info Tab: Submit Test Button -->
           <q-btn color="negative" class="float-right"  size="lg" label="SUBMIT" @click="submitButtonClicked">
           </q-btn>
-          <!-- Gen Info Tab: Submit Test Button -->
+        </q-tab-pane>
+                <!-- Criteria Tab -->
+        <q-tab-pane name="criteria">
+          <!-- Inclusion Criteria Tab Card -->
+          <q-card class="bg-cyan-2 q-ma-xl">
+            <q-card-title>Inclusion Criteria
+              <span slot="subtitle">Criteria to filter participants in the study</span>
+            </q-card-title>
+            <q-card-main>
+              <div>
+                <div>
+                <!-- Age Range & Sex -->
+                  <div class="row gutter-lg">
+                    <div class="col-xs-4 col-md-4">
+                        <q-field label="Age Range"/>
+                    </div>
+                    <div class="col-xs-4 col-md-4">
+                        <q-input v-model="ageMin" type="number" placeholder="Min Age1" clearable/>
+                    </div>
+                    <div class="col-xs-4 col-md-4">
+                        <q-input v-model="ageMax" type="number" placeholder="Max Age1" clearable/>
+                    </div>
+                    <div class="col-xs-4 col-md-4">
+                      <q-field label="Sex" />
+                    </div>
+                    <div class="col-xs-4 col-md-4">
+                      <q-checkbox class="q-mr-lg" v-model="checkArrayAge" label="M" color="secondary" val="one" />
+                      <q-checkbox class="q-mr-lg" v-model="checkArrayAge" label="F" val="two" color="secondary" />
+                      <q-checkbox v-model="checkArrayAge" label="OTHER" val="three" color="secondary" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </q-card-main>
+          </q-card>
+          <q-card class="bg-cyan-2 q-ma-xl">
+            <q-card-main>
+              <div>
+                <div>
+                  <!-- Disease, Lifestyle and Meds -->
+                  <div class="row gutter-lg">
+                    <div class="col-xs-4 col-md-4">
+                      <q-field label="Disease Choice" />
+                    </div>
+                    <div class="col-xs-4 col-md-6">
+                      <q-input type="text" id="disease-form" placeholder="Disease" />
+                    </div>
+                    <div class="col-xs-4 col-md-4">
+                      <q-field label="Lifestyle" />
+                    </div>
+                    <div class="col-xs-4 col-md-6">
+                      <q-radio v-model="radio_lifestyle" val="active" color="secondary" label="Active" />
+                      <q-radio v-model="radio_lifestyle" val="not active" color="amber" label="Not Active" style="margin-left: 10px" />
+                    </div>
+                    <div class="col-xs-4 col-md-4">
+                      <q-field label="Meds" />
+                    </div>
+                    <div class="col-xs-4 col-md-6">
+                      <q-input type="text" id="meds-form" placeholder="Meds" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </q-card-main>
+          </q-card>
+          <q-card class="bg-cyan-2 q-ma-xl">
+            <q-card-main>
+              <div>
+                <div>
+                  <!-- Custom Criteria Questions -->
+                  <div class="row gutter-lg">
+                    <div class="col-xs-4 col-md-4">
+                      <q-field label="Custom Criteria:" />
+                    </div>
+                    <div class="col-xs-4 col-md-6">
+                      <q-input type="text" id="disease-form" placeholder="Question Example:" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </q-card-main>
+          </q-card>
         </q-tab-pane>
                 <!-- TESTING Tab -->
         <q-tab-pane name="test">Test tab
@@ -192,11 +283,17 @@ export default {
           contactOfInstitution: '',
           radioDataAccessInstitution: ''
         }
-      ]
+      ],
+      ageMin: null,
+      ageMax: null,
+      checkArrayAge: ['one'],
+      radio_lifestyle: 'Active'
     }
   },
   validations: {
-    studyTitle: { required }
+    studyTitle: { required },
+    studyDescription: { required },
+    dateStart: { required }
   },
   methods: {
     addRowInvestigator (index) {
@@ -220,6 +317,12 @@ export default {
     },
     removeRowInstitution (index) {
       this.institutions.splice(index, 1)
+    },
+    checkEndDate (dateStart, dateEnd) {
+      if (Date.parse(dateStart) > Date.parse(dateEnd)) {
+        this.$q.notify('The End Date of the study is before the Start Date. Please re-enter the End Date.')
+        this.dateEnd = ''
+      }
     },
     submitButtonClicked () {
       this.$v.$touch()
