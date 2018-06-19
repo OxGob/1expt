30822945796860233ab02011d02fc29871ba5507
oxgob <arvingo@yahoo.co.uk>
2018-06-19 12:07:18 +0100

    Cleaned up some tabs

--- genexpt/src/pages/index.vue
+++ genexpt/src/pages/index.vue
@@ -3,9 +3,8 @@
       <q-tabs color="secondary" glossy align="justify">
         <q-tab slot="title" default name="generalities" icon="subject" label="Generalities" />
         <q-tab slot="title" name="criteria" icon="fingerprint" label="Inclusion Criteria" />
-        <q-tab slot="title" name="test" icon="verified_user" label="Test"/>
-        <q-tab slot="title" name="test2" icon="verified_user" label="Tab Inv"/>
-        <q-tab slot="title" name="test3" icon="verified_user" label="Tab Inst"/>
+        <q-tab slot="title" name="test" icon="verified_user" label="Tab Disease"/>
+        <q-tab slot="title" name="test2" icon="verified_user" label="Tab Inst"/>
         <!-- General Tab -->
         <q-tab-pane name="generalities">Gen tab
           <!-- General Information Tab Card -->
@@ -203,62 +202,29 @@
           </q-card>
         </q-tab-pane>
                 <!-- TESTING Tab -->
-        <q-tab-pane name="test">Test tab
+        <q-tab-pane name="test">Tab Diseases
           <q-card class="bg-cyan-2  q-ma-xl">
+          <!-- Disease, Lifestyle and Meds -->
             <q-card-main>
-            </q-card-main>
-          </q-card>
+                  <div>
+                    <div>
+                      <div class="row gutter-lg">
+                        <div class="col-xs-4 col-md-4">
+                          <q-field label="Disease Choice" />
+                        </div>
+                        <div class="col-xs-4 col-md-6">
+                          <q-input type="text" id="disease-form" placeholder="Disease" />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </q-card-main>
+            </q-card>
         </q-tab-pane>
         <!-- TESTING Tab 2 -->
-        <q-tab-pane name="test2">Tab Inv
-          <q-card class="q-ma-xl">
-            <q-card-title>Principal Investigator(s)
-              <span slot="subtitle">Information concerning the principal investigators:</span>
-            </q-card-title>
-            <q-card-main>
-              <div v-for="(principalInvestigator, index) in principalInvestigators" :key="principalInvestigator.id">
-                <div>
-                  <q-btn class="vertical-top" v-show="index !==0" round size="sm" color="negative" icon="remove" @click="removeRowInvestigator(index)" />
-                </div>
-                <q-field label="Name & Title:" helper="Principal Investigator Name & Title">
-                  <q-input v-model="principalInvestigator.nameTitle" type="text" clearable />
-                </q-field>
-                <q-field label="Contact:" helper="Contact Details, may include address">
-                  <q-input v-model="principalInvestigator.contactDetails" type="textarea" rows="4" clearable />
-                </q-field>
-                <q-field label="Institution:" helper="Name of Institution involved in study">
-                  <q-input v-model="principalInvestigator.institution" type="textarea" rows="2"  clearable />
-                </q-field>
-                <q-btn class="float-right q-mt-sm" round size="sm" color="primary" icon="add" @click="addRowInvestigator(index)" />
-                <q-card-separator class="q-mb-md q-mt-xl"/>
-              </div>
-            </q-card-main>
-          </q-card>
-        </q-tab-pane>
-        <!-- TESTING Tab 3 -->
-        <q-tab-pane name="test3">Tab Inst
-          <q-card class="q-ma-xl">
-            <q-card-title>Institution(s)
-              <span slot="subtitle">Institutions that the data might or might not be shared with:</span>
-            </q-card-title>
-            <q-card-main>
-              <div v-for="(institution, index) in institutions" :key="institution.id">
-                <q-field label="Name of Institution:" helper="Please enter the name of the institution">
-                  <q-input v-model="institution.nameOfInstitution" type="text" clearable />
-                </q-field>
-                <q-field label="Contact:" helper="Contact Details of the institution, may include address">
-                  <q-input v-model="institution.contactOfInstitution" type="textarea" rows="4" clearable />
-                </q-field>
-                <q-field class="q-mt-md" label="Data Access:" helper="Data access per institution. Please select the level of access to grant or not.">
-                  <q-radio v-model="institution.radioDataAccess" val="no" color="secondary" label="NO" />
-                  <q-radio class="q-ml-md q-mr-md" v-model="institution.radioDataAccess" val="anon" color="anon" label="ANON" />
-                  <q-radio  v-model="institution.radioDataAccess" val="full" color="full" label="FULL" />
-                </q-field>
-                <q-btn class="q-ml-md q-mt-lg" v-show="showRemoveButton" round size="sm" color="negative" icon="remove" @click="removeRowInstitution(index)" />
-                <q-btn class="float-right q-mt-lg" round size="sm" color="primary" icon="add" @click="addRowInstitution(index)" />
-                <q-card-separator class="q-mb-md q-mt-md"/>
-              </div>
-            </q-card-main>
+        <q-tab-pane name="test2">Tab ?
+          <q-card class="bg-cyan-2 q-ma-xl">
+          <q-card-main />
           </q-card>
         </q-tab-pane>
       </q-tabs>
